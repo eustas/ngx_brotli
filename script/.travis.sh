22accbded735dd5f52ccfc3b073504ac4a04dfc8
Eugene Kliuchnikov <eustas@google.com>
2018-03-20 20:39:04 +0100

    fetch sample file; run and stop nginx

--- script/.travis.sh
+++ script/.travis.sh
@@ -6,13 +6,19 @@ case "$1" in
     "install")
 	;;
     "script")
-		ROOT=`pwd`
+		ROOT=`pwd` &&
 		git clone https://github.com/nginx/nginx.git &&
-		cd nginx &&
+		cd $ROOT/script &&
+		mkdir test &&
+		cd test &&
+		curl --compressed -o war-and-peace2600.txt http://www.gutenberg.org/files/2600/2600-0.txt &&
+		cd $ROOT/nginx &&
 		./auto/configure --add-module=$ROOT/ &&
 		make &&
-		cd .. &&
-		nginx/obj/nginx
+		cd $ROOT &&
+		NGINX=$ROOT/nginx/objs/nginx &&
+		$NGINX -c $ROOT/script/test.conf &&
+		$NGINX -c $ROOT/script/test.conf -s stop
 	;;
     "after_success")
 		killall nginx
