48352299cb7be15bb20fdcf757290684fd979d93
Eugene Kliuchnikov <eustas@google.com>
2018-03-21 11:41:16 +0100

    uncompress and test file (will intentionally fail).

--- script/.travis-test.sh
+++ script/.travis-test.sh
@@ -4,13 +4,17 @@ set -e
 # Setup shortcuts.
 ROOT=`pwd`
 NGINX=$ROOT/nginx/objs/nginx
+BROTLI=$ROOT/deps/brotli/out/brotli
 SERVER=http://localhost:8080/
+FILES=$ROOT/script/test
 
 # Start server.
 $NGINX -c $ROOT/script/test.conf
 
 # Download long file with rate limit; ~5 sec.
-curl -s -H 'Accept-encoding: br' -o ./war-and-peace.br --limit-rate 300 $SERVER/war-and-peace.txt
+curl -H 'Accept-encoding: br' -o ./war-and-peace.br --limit-rate 300 $SERVER/war-and-peace.txt
+$BROTLI -dfk ./war-and-peace.br
+cmp war-and-peace.br $FILES/war-and-peace.txt
 
 # Stop server.
 $NGINX -c $ROOT/script/test.conf -s stop
