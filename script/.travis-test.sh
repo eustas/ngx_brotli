0bf5b5c2628269759be8f0de85a18e90d225d898
Eugene Kliuchnikov <eustas@google.com>
2018-03-21 11:43:50 +0100

    fix intentional failure

--- script/.travis-test.sh
+++ script/.travis-test.sh
@@ -14,7 +14,7 @@ $NGINX -c $ROOT/script/test.conf
 # Download long file with rate limit; ~5 sec.
 curl -H 'Accept-encoding: br' -o ./war-and-peace.br --limit-rate 300 $SERVER/war-and-peace.txt
 $BROTLI -dfk ./war-and-peace.br
-cmp war-and-peace.br $FILES/war-and-peace.txt
+cmp war-and-peace $FILES/war-and-peace.txt
 
 # Stop server.
 $NGINX -c $ROOT/script/test.conf -s stop
